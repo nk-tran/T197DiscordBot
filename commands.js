f97e12c39b5942a9a07e52283ac565f1ec449422
nk-tran <nktran4@gmail.com>
2023-09-27 16:57:17 -0400

    initial upload

---
+++ commands.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with index js..',
+    },
+
+    {
+        name: 'hi_function',
+        description: 'Say hi and prints interaction to console',
+    },
+
+    {
+        name: 'flip_coin',
+        description: 'Randomly returns head or tail',
+    }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Registering slash commands');
+
+        await rest.put(
+            Routes.applicationCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            { body: commands }
+        );
+
+        console.log('Slash commands started');
+    } catch (error) {
+        console.log(`There was an error: ${error}`);
+    }
+})();
---
+++ index.js
@@ -0,0 +1,52 @@
+const { Client, GatewayIntentBits  } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Returning ping interaction from commands..');
+    }
+
+    if (interaction.commandName === 'hi_function') {
+        function sayHi() {
+            interaction.reply('Hi');
+            interaction.reply(interaction);
+            console.log(interaction);
+        }
+        sayHi();
+    }
+
+    if (interaction.commandName === 'flip_coin') {
+        function flip() {
+            let val = Math.floor(Math.random()*2)
+            let result;
+            if(val === 0)
+                result = 'Heads'
+            else
+                result = 'Tails'
+            interaction.reply(result);
+        }
+        flip();
+    }
+});
+
+client.on('typingStart', async (interaction) => {
+    console.log('Started typing');
+});
+client.on('typingStop', async (interaction) => {
+    console.log('Stopped typing');
+});
+client.on('messageCreate', async (message) => {
+    if (message.content === 'ping') {
+        await message.reply('Pong!');
+    }
+});
+
+client.login(process.env.TOKEN);
