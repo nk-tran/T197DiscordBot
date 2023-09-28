633fd212243f87acb5dbdd44eb8c9f68ee0ead95
Ethan Sylvester <ethan.sylvester@gmx.com>
2023-09-28 13:22:36 -0400

    refactored admin control template script

--- commands/infomationCommands/admincontrolledcommand.js
+++ commands/infomationCommands/admincontrolledcommand.js
@@ -16,15 +16,15 @@ module.exports = {
         })
 
         // if you had the necessary role, this will be enabled
-        if (permission) {
+        if (!permission) {
+            // if you don't have permissions, go away
+            await interaction.reply(`# Sorry ${user},\n you do not have bot permissions\n # PERMISSION DENIED`);
+
+            return;
+        }
             // this line for testing to be less obnoxious on main
             // await interaction.reply(`${user} is testing a function.. pls look away, its embarrassing...`);
-            await interaction.reply(`${user} you have permission to write to bots`);
-
-        } else {
-            // if you don't have permissions, go away
-            await interaction.reply(`# Sorry ${user},\n do not have bot permissions`);
-        }
-
+            
+            await interaction.reply(`${user} you have permission to write to bots\n # PERMISSION GRANTED`);
     } 
 }
