b6553415d507bd412830b977e8a6f71e90a28e5c
nk-tran <nktran4@gmail.com>
2023-09-28 13:17:25 -0400

    revert

---
+++ commands/infomationCommands/admincontrolledcommand.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('am_i_admin')
+        .setDescription('Provides information about the user.'),
+    async execute(interaction) {
+        const user = interaction.user.username;
+        const role = interaction.member.roles.cache
+        let permission = false;
+
+        // checks each of your roles to see if any of your toles match Botters or trust
+        role.forEach(element => {
+            if (element.name === "Botters" || element.name === "trust")
+            permission = true
+        })
+
+        // if you had the necessary role, this will be enabled
+        if (permission) {
+            // this line for testing to be less obnoxious on main
+            // await interaction.reply(`${user} is testing a function.. pls look away, its embarrassing...`);
+            await interaction.reply(`${user} you have permission to write to bots`);
+
+        } else {
+            // if you don't have permissions, go away
+            await interaction.reply(`# Sorry ${user},\n do not have bot permissions`);
+        }
+
+    } 
+}
