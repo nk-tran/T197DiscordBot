e2a74257f34e2b0d0bcd304b40ed1b8e401dd92f
Ethan Sylvester <ethan.sylvester@gmx.com>
2023-09-28 11:24:36 -0400

    commented the admincommand to make it more clear

--- commands/infomationCommands/admincontrolledcommand.js
+++ commands/infomationCommands/admincontrolledcommand.js
@@ -5,6 +5,9 @@ module.exports = {
         .setName('admintest')
         .setDescription('Provides information about the user.'),
     async execute(interaction) {
+        //                  This bitfield number: 114349209288703n equates to Admin permission
+        // if user has admin bitfield in the channel the command was called from, reply with admin 
+        // function, otherwise, tell them to get lost
         if (interaction.member.permissionsIn(interaction.channel).bitfield == 114349209288703n) {
             await interaction.reply(`You have administrator permissions`);
         } else {
