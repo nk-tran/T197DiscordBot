6cad456c8485701ae661810305168ff3e761c753
Ethan Sylvester <ethan.sylvester@gmx.com>
2023-09-28 13:25:16 -0400

    .env.example

--- commands/infomationCommands/admincontrolledcommand.js
+++ commands/infomationCommands/admincontrolledcommand.js
@@ -18,13 +18,12 @@ module.exports = {
         // if you had the necessary role, this will be enabled
         if (!permission) {
             // if you don't have permissions, go away
-            await interaction.reply(`# Sorry ${user},\n you do not have bot permissions\n # PERMISSION DENIED`);
-
+            await interaction.reply(`# PERMISSION DENIED \nSorry ${user}, you do not have bot permissions\n`);
             return;
         }
             // this line for testing to be less obnoxious on main
             // await interaction.reply(`${user} is testing a function.. pls look away, its embarrassing...`);
-            
-            await interaction.reply(`${user} you have permission to write to bots\n # PERMISSION GRANTED`);
+
+            await interaction.reply(` # PERMISSION GRANTED \n${user} you have permission to write to bots\n`);
     } 
 }
